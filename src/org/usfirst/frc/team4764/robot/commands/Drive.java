077f1ea2280c564031252db6f5d38a9acc4aba60
rbarne17 <rbarne17@laptop-rbarne17.uthsc.edu>
2018-01-28 14:35:39 -0600

    first commit from Roy branch.

---
+++ src/org/usfirst/frc/team4764/robot/commands/Drive.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team4764.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Drive extends Command {
+
+	public Drive() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Drive(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Drive(double timeout) {
+		super(timeout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Drive(String name, double timeout) {
+		super(name, timeout);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
