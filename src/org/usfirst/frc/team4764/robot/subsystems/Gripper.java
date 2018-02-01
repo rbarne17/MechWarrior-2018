0aaaf466ba47ee969b6d2cb8f8f154712aed91a9
MMW 4764 <MMW 4764@LAPTOP-R5MJBNQT>
2018-01-31 18:54:04 -0600

    Created the necessary commands and subsystems stubs.

---
+++ src/org/usfirst/frc/team4764/robot/subsystems/Gripper.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team4764.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Gripper extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
