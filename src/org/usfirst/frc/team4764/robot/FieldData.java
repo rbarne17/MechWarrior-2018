cb664216661d0d71d4b5886bfc580a52879fe591
rbarne17 <rbarne17@laptop-rbarne17.uthsc.edu>
2018-02-08 16:45:41 -0600

    changed AutonomousPath to AutonomousPathName

--- src/org/usfirst/frc/team4764/robot/FieldData.java
+++ src/org/usfirst/frc/team4764/robot/FieldData.java
@@ -6,15 +6,15 @@ public class FieldData {
 	public static DriverStation.Alliance Alliance = DriverStation.getInstance().getAlliance();
 	public static String AllianceOrder = DriverStation.getInstance().getGameSpecificMessage();
 	public static int Location = DriverStation.getInstance().getLocation();
-	public static String AutonomousPath;
+	public static String AutonomousPathName;
 
 	public FieldData() {
 		switch (Integer.toString(Location) + AllianceOrder) {
 		case "1LLL":
-			AutonomousPath = "1LLL";
+			AutonomousPathName = "1LLL";
 			break;
 		default:
-			AutonomousPath = "";
+			AutonomousPathName = "";
 			break;
 
 		}
