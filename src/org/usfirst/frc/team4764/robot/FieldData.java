78c2ea97e3cbc1cf6696b131ac042c8bc44a36e7
rbarne17 <rbarne17@laptop-rbarne17.uthsc.edu>
2018-02-10 14:41:19 -0600

    added variables for position of Switch and Scale.

--- src/org/usfirst/frc/team4764/robot/FieldData.java
+++ src/org/usfirst/frc/team4764/robot/FieldData.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.DriverStation;
 public class FieldData {
 	public static DriverStation.Alliance Alliance = DriverStation.getInstance().getAlliance();
 	public static String AllianceOrder = DriverStation.getInstance().getGameSpecificMessage();
+	public static char SwitchSide = AllianceOrder.charAt(1);
+	public static char ScaleSide = AllianceOrder.charAt(2);
 	public static int Location = DriverStation.getInstance().getLocation();
 
 	public FieldData() {
