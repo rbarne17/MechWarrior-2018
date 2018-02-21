5305fb0d24bcbe6ce1da7f471b9e6e4fe3931db6
rbarne17 <rbarne17@laptop-rbarne17.uthsc.edu>
2018-02-20 18:43:58 -0600

    began adding commands for smart dashboard.

--- src/org/usfirst/frc/team4764/robot/SmartDashboard.java
+++ src/org/usfirst/frc/team4764/robot/SmartDashboard.java
@@ -2,4 +2,13 @@ package org.usfirst.frc.team4764.robot;
 
 public class SmartDashboard {
 
+	
+	public static void teleopPeriodic()
+	{
+		
+	}
+	
+	public static void autonomousPeriodic() {
+		
+	}
 }
