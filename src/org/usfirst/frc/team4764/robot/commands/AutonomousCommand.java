9ac458f26699e3a43c3622745b9f39cd96cc19ee
MMW 4764 <MMW 4764@LAPTOP-R5MJBNQT>
2018-02-20 18:06:28 -0600

    pseudo code for the scale in the autonomous code.

--- src/org/usfirst/frc/team4764/robot/commands/AutonomousCommand.java
+++ src/org/usfirst/frc/team4764/robot/commands/AutonomousCommand.java
@@ -24,30 +24,33 @@ public class AutonomousCommand extends CommandGroup {
 	public AutonomousCommand(String autonomousCommandName) {
 		switch (autonomousCommandName) {
 		case "OneLeftSwitchScore":
-			//? ft to drive foward
 			;
 			break;
 		case "OneLeftSwitchWaitScore":
-			//? ft to drive foward
 			;
 			break;
 		case "OneRightSwitchDefend":
-			//? ft to drive foward
 			;
 			break;
 		case "OneLeftScaleScore":
-			//Drive 13.5 ft straight
-			// raise lifter 5.5 ft high
+			//drive foward 275ft
+			//raise the lifter 5.5 high
+			//turn 90 degrees to the right
+			//drive foward 1ft
+			//drop the cube
 			;
 			break;
 		case "OneLeftScaleWaitScore":
-			//Drive 13.5 ft straight
-			//raise lifter 5.5 ft high
+			//wait 2.5 seconds
+			//drive foward 27ft
+			//raise the lifter 5.5 high
+			//turn 90 degrees to the right
+			//drive foward 1ft
+			//drop the cube
 			;
 			break;
 		case "OneRightScaleDefend":
-			//raise lifter 5.5 ft high
-			//Drive 13.5 ft straight
+			//Waiting for strategy			
 			;
 			break;
 		case "TwoLeftSwitchScore":
@@ -69,21 +72,61 @@ public class AutonomousCommand extends CommandGroup {
 			;
 			break;
 		case "TwoLeftScaleScore":
+			//drive 5ft foward
+			//turn 90 degrees to the left
+			//drive 3ft foward
+			//turn 90 degrees to the right
+			//drive foward 22ft
+			//turn 90 degrees to the right
+			//raise lifter 5.5 ft high
+			//drive foward 1ft
+			//drop cube
 			;
 			break;
 		case "TwoLeftScaleWaitScore":
+			// wait 2.5 seconds
+			//drive 5ft foward
+			//turn 90 degrees to the left
+			//drive 3ft foward
+			//turn 90 degrees to the right
+			//drive foward 22ft
+			//turn 90 degrees to the right
+			//raise lifter 5.5 ft high
+			//drive foward 1ft
+			//drop cube
 			;
 			break;
 		case "TwoLeftScaleDefend":
+			//Waiting for strategy
 			;
 			break;
 		case "TwoRightScaleScore":
+			//drive 5ft foward
+			//turn 90 degrees to the right
+			//drive 3ft foward
+			//turn 90 degrees to the left
+			//drive foward 22ft
+			//turn 90 degrees to the left
+			//raise lifter 5.5 ft high
+			//drive foward 1ft
+			//drop cube
 			;
 			break;
 		case "TwoRightScaleWaitScore":
+			//wait 2.5 seconds
+			//drive 5ft foward
+			//turn 90 degrees to the left
+			//drive 3ft foward
+			//turn 90 degrees to the right
+			//drive foward 22ft
+			//turn 90 degrees to the right
+			//raise lifter 5.5 ft high
+			//drive foward 1ft
+			//drop cube
 			;
 			break;
 		case "TwoRightScaleDefend":
+			//Waiting for strategy
 			;
 			break;
 		case "ThreeRightSwitchSore":
@@ -96,12 +139,23 @@ public class AutonomousCommand extends CommandGroup {
 			;
 			break;
 		case "ThreeRightScaleScore":
+			//drive foward 27ft
+			//raise the lifter 5.5 high
+			//turn 90 degrees to the left
+			//drive foward 1ft
+			//drop the cube
 			;
 			break;
 		case "ThreeRightScaleWaitScore":
+			//drive foward 27ft
+			//raise the lifter 5.5 high
+			//turn 90 degrees to the left
+			//drive foward 1ft
+			//drop the cube
 			;
 			break;
 		case "ThreeLeftScaleDefend":
+			//Waiting for strategy
 			;
 			break;
 		}
