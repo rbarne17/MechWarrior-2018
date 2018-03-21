8d8b8015539ffe15e905ecc38837444824c307f9
rbarne17 <rbarne17@laptop-rbarne17.uthsc.edu>
2018-03-20 19:24:36 -0500

    fixed conflicting DIO ports between lift and gripper.

--- src/org/usfirst/frc/team4764/robot/RobotMap.java
+++ src/org/usfirst/frc/team4764/robot/RobotMap.java
@@ -26,8 +26,8 @@ public final class RobotMap {
 
 	// limit switches
 	public static final int liftLimitSwitchHome = 8;
-	public static final int gripperLimitSwitchClose=4;
-	public static final int gripperLimitSwitchOpen=5;
+	public static final int gripperLimitSwitchClose=9;
+	public static final int gripperLimitSwitchOpen=10;
 	public static final int flipityFlopLimitSwitchUp=7;
 	public static final int flipityFlopLimitSwitchLevel=6;
 	
