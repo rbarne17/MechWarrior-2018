9618618dad3555505756b7580dc7c3e2be14193c
Barnes <rbarne17@uthsc.edu>
2018-03-21 18:15:08 -0400

    Revert "Fixed FlyWheel" to stick with one spark. Still in history.

--- src/org/usfirst/frc/team4764/robot/subsystems/FlyWheel.java
+++ src/org/usfirst/frc/team4764/robot/subsystems/FlyWheel.java
@@ -2,8 +2,6 @@ package org.usfirst.frc.team4764.robot.subsystems;
 
 import org.usfirst.frc.team4764.robot.RobotMap;
 import org.usfirst.frc.team4764.robot.commands.FlipityFlopWithController;
-import org.usfirst.frc.team4764.robot.commands.GrabCube;
-
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -15,28 +13,23 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class FlyWheel extends Subsystem {
-	Spark flyWheelMotorRight = new Spark(RobotMap.flyWheelMotorRight);
-	Spark flyWheelMotorLeft = new Spark(RobotMap.flyWheelMotorLeft);
+	Spark flyWheelMotor = new Spark(RobotMap.flyWheelMotor);
 
 	public FlyWheel() {
-		flyWheelMotorRight.set(0.0);
-		flyWheelMotorLeft.set(0.0);
+		flyWheelMotor.set(0.0);
 
 	}
 
 	public void flyWheelAdjust(double speed) {
-		flyWheelMotorRight.set(speed);
-		flyWheelMotorLeft.set(-speed);
+		flyWheelMotor.set(speed);
 	}
 
 	public void reset() {
-		flyWheelMotorRight.set(0.0);
-		flyWheelMotorLeft.set(0.0);
+		flyWheelMotor.set(0.0);
 	}
 
 	@Override
 	protected void initDefaultCommand() {
-		new GrabCube();
 	}
 
 }
